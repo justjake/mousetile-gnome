17a5584233e52420925d97df519d85027bd3fc83
Jake Teton-Landis <just.1.jake@gmail.com>
2012-12-28 21:45:10 -0800

    moved coffee to src, JS libraries will live in named directory Mousetile

--- watch.zsh
+++ watch.zsh
@@ -4,5 +4,5 @@ function this-script-dir {
     echo "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 }
 
-# Watch anc compile the source directory
-coffee --output "$(this-script-dir)/" --bare --watch --compile "$(this-script-dir)/src/"
+# Watch and compile the source directory
+coffee --output "$(this-script-dir)/Mousetile/" --bare --watch --compile "$(this-script-dir)/src/"
